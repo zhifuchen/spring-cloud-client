861f09054b1f76f81cd8415fb538e4eee0717d66
zhifu.chen <zhifu.chen@fu-fang.com>
2017-07-04 14:09:42 +0800

    引入hystrix

--- src/main/java/com/fufang/cloud/service/ComputeClient.java
+++ src/main/java/com/fufang/cloud/service/ComputeClient.java
@@ -3,11 +3,21 @@ package com.fufang.cloud.service;
 import com.fufang.cloud.model.WithdrawApply;
 import com.fufang.cloud.util.core.response.FFApiResponse;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("compute-service")
+import static com.fufang.cloud.util.core.response.FFApiResponse.success;
+
+@FeignClient(value = "compute-service", fallback = ComputeClient.HystrixClientFallback.class)
 public interface ComputeClient {
     @RequestMapping(method = RequestMethod.POST, value = "/withdrawApply/addApply")
     FFApiResponse<String> addApply(WithdrawApply withdrawApply);
+    @Component
+    class HystrixClientFallback implements ComputeClient {
+        @Override
+        public FFApiResponse<String> addApply(WithdrawApply withdrawApply) {
+            return success("fallback");
+        }
+    }
 }
